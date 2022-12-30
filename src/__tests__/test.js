bf7066c73231b61221dc77226ecae7c72dfe8387
Pavel Chekalin <grokki91@gmail.com>
2022-12-30 16:15:02 +0300

    Add tests

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,1 +1,20 @@
-import Validator from "../app/Validator";
+import Validator from '../app/Validator';
+
+describe(('Check function Validator'), () => {
+  test(('Check correct validation'), () => {
+    const validation = new Validator('Gamer');
+    expect(validation.validateUserName()).toBeTruthy();
+  });
+  test(('Check empty validation'), () => {
+    const validation = new Validator('');
+    expect(validation.validateUserName()).toBeFalsy();
+  });
+  test(('Check first symbol "-" validation'), () => {
+    const validation = new Validator('-Gamer');
+    expect(validation.validateUserName()).toBeFalsy();
+  });
+  test(('Check end symbol "4" validation'), () => {
+    const validation = new Validator('SuperPlayer4');
+    expect(validation.validateUserName()).toBeFalsy();
+  });
+});
